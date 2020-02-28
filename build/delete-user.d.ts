0e160ed929aa8ef911be604437c136708a298c3a
James-Alex Matthews <vamtiger@vamtiger.com>
2020-02-28 12:03:55 +0200

    vamtiger-node-typescript-commit

--- build/delete-user.d.ts
+++
@@ -1,3 +0,0 @@
-import { IDeleteUser } from './types';
-export default function ({ element }: IDeleteUser): Promise<void>;
-export declare function confirm({ element, userElement }: IDeleteUser): void;
