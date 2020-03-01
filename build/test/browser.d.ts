06dd95d3cd7851b86cbe76c1fcc983e2e7756a77
James-Alex Matthews <vamtiger@vamtiger.com>
2020-03-01 16:42:52 +0200

    vamtiger-node-typescript-commit

--- build/test/browser.d.ts
+++
@@ -1,5 +0,0 @@
-import VamtigerUserRegistry from '../element';
-export declare function getElement(newElement?: boolean): Promise<VamtigerUserRegistry | null>;
-export declare function removeUsers({ element }: {
-    element: VamtigerUserRegistry;
-}): void;
